e3a724a72f8330f489f2ce46fefe4c648951a612
Sarvajeet <sarvajeet.singh@rnf.tech>
2025-08-12 19:04:33 +0530

    Updated ai agent

--- index.js
+++ index.js
@@ -3,15 +3,32 @@ import { ChatOpenAI } from "@langchain/openai"
 import {z} from "zod"
 import dotenv from "dotenv"
 
+import { tavily } from "@tavily/core";
+
 dotenv.config();
 
-
+const client =  tavily({apiKey : process.env.TAVILYAPIKEY})
 
 
 const llm = new ChatOpenAI({
   apiKey : process.env.OPENAIKEY,
   model : "gpt-4o"
 })
+
+const search = tool(
+  async( {query} ) => {
+    const result = await client.search(query , {maxResult : 5})
+    return result
+  }, 
+  {
+    name : "websearch", 
+    description : "searches the web for up-to-date information",
+    schema : z.object({
+      query : z.string()
+    })
+
+  }
+)
 
 const add = tool(
   async( {a , b} ) => {
@@ -28,10 +45,14 @@ const add = tool(
   }
 )
 
-const llmWithTools = llm.bindTools([add])
-const message = await llmWithTools.invoke("What is the sum of 8 and 4?");
+const llmWithSearch = llm.bindTools([search])
 
-console.log("message" , message)
+
+const llmWithTools = llm.bindTools([add])
+const message = await llmWithTools.invoke("What is the sum of 242 and 23?");
+const searchMessage = await llmWithSearch.invoke("what is the name of indian prime minister?")
+console.log("searchMessage" , searchMessage)
+
 
 
 if(message?.tool_calls.length){
@@ -52,3 +73,23 @@ if(message?.tool_calls.length){
     }
   }
 }
+
+if (searchMessage?.tool_calls?.length) {
+  for (const call of searchMessage.tool_calls) {
+    if (call.name === "websearch") {
+      const result = await search.invoke(call.args);
+
+      const finalSearchAnswer = await llmWithSearch.invoke([
+        searchMessage,
+        {
+          role: "tool",
+          tool_call_id: call.id,
+          content: JSON.stringify(result)
+        }
+      ]);
+
+      console.log("Final Search Answer:", finalSearchAnswer);
+    }
+  }
+}
+
